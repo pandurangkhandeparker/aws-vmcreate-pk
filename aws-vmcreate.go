06fd3c9842884e8fd36f45c50e39fe051c1e2eb4
manoj <manoj.dhanorkar@gmail.com>
2023-01-19 10:29:03 +0000

    updated dockerfile and added delete instance code

--- aws-vmcreate.go
+++ aws-vmcreate.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"flag"
-
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -24,6 +24,14 @@ type EC2CreateInstanceAPI interface {
 	CreateTags(ctx context.Context,
 		params *ec2.CreateTagsInput,
 		optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error)
+}
+
+// EC2TerminateInstanceAPI defines the interface for the TerminateInstances functions.
+// We use this interface to test the functions using a mocked service.
+type EC2TerminateInstanceAPI interface {
+	TerminateInstances(ctx context.Context,
+		params *ec2.TerminateInstancesInput,
+		optFns ...func(*ec2.Options)) (*ec2.TerminateInstancesOutput, error)
 }
 
 // MakeInstance creates an Amazon Elastic Compute Cloud (Amazon EC2) instance.
@@ -55,8 +63,101 @@ func MakeInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.RunIns
 func MakeTags(c context.Context, api EC2CreateInstanceAPI, input *ec2.CreateTagsInput) (*ec2.CreateTagsOutput, error) {
 	return api.CreateTags(c, input)
 }
-func DeleteInstancesCmd() {
-	fmt.Println("TO DO")
+
+// DeleteInstance terminates an Amazon Elastic Compute Cloud (Amazon EC2) instance.
+// Inputs:
+//
+//	c is the context of the method call, which includes the AWS Region.
+//	api is the interface that defines the method call.
+//	input defines the input arguments to the service call.
+//
+// Output:
+//
+//	If success, a TerminateInstancesOutput object containing the result of the service call and nil.
+//	Otherwise, nil and an error from the call to TerminateInstances.
+func DeleteInstance(c context.Context, api EC2TerminateInstanceAPI, input *ec2.TerminateInstancesInput) (*ec2.TerminateInstancesOutput, error) {
+	return api.TerminateInstances(c, input)
+}
+
+// func DeleteInstancesCmd(instanceIds []string) {
+// 	// fmt.Println("TO DO")
+
+// 	input := &ec2.TerminateInstancesInput{
+// 		InstanceIds: instanceIds,
+// 		DryRun:      new(bool),
+// 	}
+
+// 	result, err := DeleteInstance(context.TODO(), client, input)
+// 	if err != nil {
+// 		fmt.Println("Got an error terminating the instance:")
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	fmt.Println("Terminated instance with id: ", *result.TerminatingInstances[0].InstanceId)
+// }
+
+func DeleteInstancesCmd(name *string, value *string) {
+	// fmt.Println("TO DO")
+
+	// tagInput := &ec2.DeleteTagsInput{
+	// 	Resources: []string{*result.Instances[0].InstanceId},
+	// 	Tags: []types.Tag{
+	// 		{
+	// 			Key:   name,
+	// 			Value: value,
+	// 		},
+	// 	},
+	// }
+
+	// _, err = RemoveTags(context.TODO(), client, tagInput)
+	// if err != nil {
+	// 	fmt.Println("Got an error tagging the instance:")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	var instanceIds = make([]string, 0)
+
+	val := strings.Split(*value, ",")
+	tag := "tag:" + *name
+
+	input1 := &ec2.DescribeInstancesInput{
+		Filters: []types.Filter{
+			{
+				Name:   aws.String(tag),
+				Values: val,
+			},
+		},
+	}
+	result, err := client.DescribeInstances(context.TODO(), input1)
+	if err != nil {
+		fmt.Println("Got an error fetching the status of the instance")
+		fmt.Println(err)
+	} else {
+		for _, r := range result.Reservations {
+			fmt.Println("Instance IDs:")
+			for _, i := range r.Instances {
+				//value := *i.InstanceId
+				instanceIds = append(instanceIds, *i.InstanceId)
+			}
+			fmt.Println(instanceIds)
+		}
+
+		input := &ec2.TerminateInstancesInput{
+			InstanceIds: instanceIds,
+			DryRun:      new(bool),
+		}
+
+		result, err := DeleteInstance(context.TODO(), client, input)
+		if err != nil {
+			fmt.Println("Got an error terminating the instance:")
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Terminated instance with id: ", *result.TerminatingInstances[0].InstanceId)
+	}
 }
 
 func CreateInstancesCmd(name *string, value *string) {
@@ -64,7 +165,7 @@ func CreateInstancesCmd(name *string, value *string) {
 	minMaxCount := int32(1)
 
 	input := &ec2.RunInstancesInput{
-		ImageId:      aws.String("ami-0b5eea76982371e91"),
+		ImageId:      aws.String("ami-0d0ca2066b861631c"),
 		InstanceType: types.InstanceTypeT2Micro,
 		MinCount:     &minMaxCount,
 		MaxCount:     &minMaxCount,
@@ -109,6 +210,7 @@ func main() {
 	command := flag.String("c", "", "command  create or delete")
 	name := flag.String("n", "", "The name of the tag to attach to the instance")
 	value := flag.String("v", "", "The value of the tag to attach to the instance")
+	// instanceId := flag.String("i", "", "The IDs of the instance to terminate")
 
 	flag.Parse()
 
@@ -122,11 +224,18 @@ func main() {
 		return
 	}
 
+	// if *instanceId == "" {
+	// 	fmt.Println("You must supply an instance ID (-i INSTANCE-ID or comma separated list of ids")
+	// 	return
+	// }
+
+	// instances := strings.Split(*instanceId, ",")
+
 	if *command == "create" {
 		CreateInstancesCmd(name, value)
 	}
 
 	if *command == "delete" {
-		DeleteInstancesCmd()
+		DeleteInstancesCmd(name, value)
 	}
 }
